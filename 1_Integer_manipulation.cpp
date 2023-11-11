1c3d47cb3c26132684552bf266f22ff0b9d3294c
Mejtus1 <GitMaster1769@proton.me>
2023-11-11 17:28:05 +0100

    s

---
+++ 1_Integer_manipulation.cpp
@@ -0,0 +1,86 @@
+// Integer type size 
+// size associated with integer type depends on compiler
+/* 
+char, unsigned char == 16 bits
+short, unsigned short == 16 bits
+int, unsigned int == 32 bits
+long, unsigned long == 64 bits */
+
+sizeof ( int ) // 
+
+/*
+COMPILERS 
+Linux OS 
+Windows OS 64 bit 
+Windows OS 32 bit 
+
+///////////////////////////////////////
+Size of LINUX 64 bit edition data types 
+sizeof(bool) = 1 byte
+sizeof(char) = 1 byte
+sizeof(unsigned char) = 1 byte
+sizeof(short) = 2 bytes
+sizeof(unsigned short) = 2 bytes
+sizeof(int) = 4 bytes
+sizeof(unsigned int) = 4 bytes
+sizeof(long) = 8 bytes
+sizeof(unsigned long) = 8 bytes
+sizeof(long long) = 8 bytes
+sizeof(unsigned long long) = 8 bytes
+
+sizeof(int8_t) = 1 byte
+sizeof(uint8_t) = 1 byte
+sizeof(int16_t) = 2 bytes
+sizeof(uint16_t) = 2 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int64_t) = 8 bytes
+sizeof(uint64_t)= 8 bytes
+
+///////////////////////////////////////////////
+Size of Windows x86 = 32 bit edition data types
+sizeof(bool) = 1 byte
+sizeof(char) = 1 byte
+sizeof(unsigned char) = 1 byte
+sizeof(short) = 2 bytes
+sizeof(unsigned short) = 2 bytes
+sizeof(int) = 4 bytes
+sizeof(unsigned int) = 4 bytes
+sizeof(long) = 4 bytes // here is change other than linux 
+sizeof(unsigned long) = 4 bytes // here is change other than linux 
+sizeof(long long) = 8 bytes
+sizeof(unsigned long long) = 8 bytes
+
+sizeof(int8_t) = 1 byte
+sizeof(uint8_t) = 1 byte
+sizeof(int16_t) = 2 bytes
+sizeof(uint16_t) = 2 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int64_t) = 8 bytes
+sizeof(uint64_t)= 8 bytes
+
+/////////////////////////////////////////
+Size of WINDOWS 64 bit edition data types
+sizeof(bool) = 1 byte
+sizeof(char) = 1 byte
+sizeof(unsigned char) = 1 byte
+sizeof(short) = 2 bytes
+sizeof(unsigned short) = 2 bytes
+sizeof(int) = 4 bytes
+sizeof(unsigned int) = 4 bytes
+sizeof(long) = 4 bytes // here is change other than linux 
+sizeof(unsigned long) = 4 bytes // here is change other than linux 
+sizeof(long long) = 8 bytes
+sizeof(unsigned long long) = 8 bytes
+
+sizeof(int8_t) = 1 byte
+sizeof(uint8_t) = 1 byte
+sizeof(int16_t) = 2 bytes
+sizeof(uint16_t) = 2 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int32_t) = 4 bytes
+sizeof(int64_t) = 8 bytes
+sizeof(uint64_t)= 8 bytes
+
+//look out for compiling signed and unsigned value in C++
